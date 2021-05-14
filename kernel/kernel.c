847add88686143900a83d68b0a1b943b13687b85
Alex Sobiek <alex@alexsobiek.com>
2021-05-13 22:29:58 -0500

    Began working on keyboard input handling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,16 @@
 // ===========================================================================
 // Super simple operating system kernel
 // Authored by Alexander Sobiek, 2021
-//===========================================================================
+// ===========================================================================
 
 # include "kernel.h"
 # include "../drivers/vga/vga.h"
+# include "../drivers/keyboard/keyboard.h"
+# include "../lib/stdio/stdio.h"
+
+void handleInput() {
+
+}
 
 /**
  * Kernel entry point
@@ -14,5 +20,6 @@ int main() {
     setVGAColorScheme(GREY, BLUE);
     initDefaultVGA();
     printMOTD();
+    handleInput();
     return 0;
 }
